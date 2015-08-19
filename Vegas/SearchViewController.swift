3fad319ccfa4ab7b32051307ce7cb8bbb051e664
Chris Archibald <carchi8py@gmail.com>
2015-08-18 21:44:32 -0700

    Added Some comment to the code.

--- Vegas/SearchViewController.swift
+++ Vegas/SearchViewController.swift
@@ -34,24 +34,29 @@ class SearchViewController: UIViewController {
         sender.inputView = datePickerView
         datePickerView.addTarget(self, action: Selector("datePickerValueChanged:"), forControlEvents: UIControlEvents.ValueChanged)
     }
-    
-    func datePickerValueChanged(sender:UIDatePicker) {
-        var dateFormatter = NSDateFormatter()
-        dateFormatter.dateStyle = NSDateFormatterStyle.MediumStyle
-        dateFormatter.timeStyle = NSDateFormatterStyle.NoStyle
-        if isOut {
-            outTextField.text = dateFormatter.stringFromDate(sender.date)
-        } else {
-            inTextField.text = dateFormatter.stringFromDate(sender.date)
-        }
-    }
 
+    //When the user clicks on the Check out text field a Date Picker will appear 
+    //At the bottom of the screen to pick a date
     @IBAction func outTextFiledEdit(sender: UITextField) {
         isOut = true
         var datePickerView:UIDatePicker = UIDatePicker()
         datePickerView.datePickerMode = UIDatePickerMode.Date
         sender.inputView = datePickerView
         datePickerView.addTarget(self, action: Selector("datePickerValueChanged:"), forControlEvents: UIControlEvents.ValueChanged)
+    }
+    
+    //Take the date and moved it to the textfield
+    func datePickerValueChanged(sender:UIDatePicker) {
+        var dateFormatter = NSDateFormatter()
+        dateFormatter.dateStyle = NSDateFormatterStyle.MediumStyle
+        dateFormatter.timeStyle = NSDateFormatterStyle.NoStyle
+        //because we are using the same textfield, we have a bolean vaule that
+        //will tell us which field to save the vaule to
+        if isOut {
+            outTextField.text = dateFormatter.stringFromDate(sender.date)
+        } else {
+            inTextField.text = dateFormatter.stringFromDate(sender.date)
+        }
     }
 }
 
