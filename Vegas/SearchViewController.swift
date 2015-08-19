5572c6fac8d268d6b43541992121b70342d03c93
Chris Archibald <carchi8py@gmail.com>
2015-08-18 21:29:49 -0700

    Have check in and check out date selected

--- Vegas/SearchViewController.swift
+++ Vegas/SearchViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class SearchViewController: UIViewController {
-
+    
+    var isOut = false
+    @IBOutlet weak var inTextField: UITextField!
+    @IBOutlet weak var outTextField: UITextField!
+    @IBOutlet weak var dateField: UIDatePicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,33 @@ class SearchViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    //When the user click on the Check in Text Field a Date picker will appear
+    //At the bottom of the screen to pick a date
+    @IBAction func inTextFieldEdit(sender: UITextField) {
+        isOut = false
+        var datePickerView:UIDatePicker = UIDatePicker()
+        datePickerView.datePickerMode = UIDatePickerMode.Date
+        sender.inputView = datePickerView
+        datePickerView.addTarget(self, action: Selector("datePickerValueChanged:"), forControlEvents: UIControlEvents.ValueChanged)
+    }
+    
+    func datePickerValueChanged(sender:UIDatePicker) {
+        var dateFormatter = NSDateFormatter()
+        dateFormatter.dateStyle = NSDateFormatterStyle.MediumStyle
+        dateFormatter.timeStyle = NSDateFormatterStyle.NoStyle
+        if isOut {
+            outTextField.text = dateFormatter.stringFromDate(sender.date)
+        } else {
+            inTextField.text = dateFormatter.stringFromDate(sender.date)
+        }
+    }
 
+    @IBAction func outTextFiledEdit(sender: UITextField) {
+        isOut = true
+        var datePickerView:UIDatePicker = UIDatePicker()
+        datePickerView.datePickerMode = UIDatePickerMode.Date
+        sender.inputView = datePickerView
+        datePickerView.addTarget(self, action: Selector("datePickerValueChanged:"), forControlEvents: UIControlEvents.ValueChanged)
+    }
 }
 
